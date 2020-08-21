7894691bc3b471982cff1a9f726d520c0a7dabd8
Jason <jason@theremotecreative.com>
2020-08-21 17:42:26 -0500

    testing gsap v6

--- src/components/home-sections/skill-focused-line.js
+++ src/components/home-sections/skill-focused-line.js
@@ -1,67 +1,43 @@
 import React, { Component } from "react"
 import styled from 'styled-components'
-import { TweenLite, TimelineLite, CSSPlugin } from 'gsap';
 
-//without this line, CSSPlugin and AttrPlugin may get dropped by your bundler...
-const plugins = [ CSSPlugin, TweenLite ];
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+if (typeof window !== `undefined`) {
+  gsap.registerPlugin(ScrollTrigger)
+  gsap.core.globals("ScrollTrigger", ScrollTrigger)
+}
 
 class SkillLine extends Component {
 
-    constructor(props){
-		super(props);
-		// logo container
-		this.logoContainer = null;
-		// logo tween
-		this.logoTween = null;
-	}
-
-
-	componentDidMount(){
-		// create logo tween
-		this.logoTween = new TimelineLite({ paused:true })
-			.to(this.logoContainer, 2, { x: 500 })
-			.to(this.logoContainer, 1, { rotation: 360, transformOrigin: "center" });
-	}
+    constructor(props) {
+        super(props);
+        this.container = null;
+        this.trigger = null;
+        this.tl = gsap.timeline({
+          paused: true,
+          scrollTrigger: {
+            trigger: "#trigger",
+            scrub: true,
+            markers: true,
+          }
+        });
+      }
+      componentDidMount() {
+        this.tl.to(this.container, {
+            width: '80%',
+        });
+      }
 
     render() {
 
         return (
             <MainContainer>
 
-                <div className="mb-2 btn-group">
-                    <button
-                        className="btn gsap-btn"
-                        onClick={() => this.logoTween.play()}
-                    >Play</button>
-                    <button
-                        className="btn gsap-btn"
-                        onClick={() => this.logoTween.pause()}
-                    >Pause</button>
-                    <button
-                        className="btn gsap-btn"
-                        onClick={() => this.logoTween.reverse()}
-                    >Reverse</button>
-                    <button
-                        className="btn gsap-btn"
-                        onClick={() => this.logoTween.restart()}
-                    >Restart</button>
+                <div id="trigger" ref={div => (this.trigger = div)} style={{ background: '#fff', height: '140px', width: '100%', position: 'relative' }}>
+                        <Line ref={div => (this.container = div)}/>
                 </div>
-
-                <img
-                    src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/logo-man.svg"
-                    alt=""
-                    className="img-fluid logo"
-                    ref={ img => this.logoContainer = img }
-                />
-                
-                <LineContainer>
-                    <svg viewBox="0 0 1000 50" id="line_box">
-                        <path
-                        style={{ fill: 'none', stroke: '#000000', strokeWidth: '30', }}
-                        d="M 0,10 H 1000"
-                        id="skill_line" />
-                    </svg>
-                </LineContainer> 
 
             </MainContainer>
         );
@@ -70,27 +46,22 @@ class SkillLine extends Component {
 
 }
 
+const Line = styled.div`
+    width: 0;
+    height: 40px;
+    background-color: #000;
+    position: absolute;
+    top: 20px;
+    left: 20px;
+`
+
+
 const MainContainer = styled.div`
     max-width: 1340px;
     padding-right: 20px;
     padding-left: 20px;
     width: 100%;
     margin: 0 auto;
-`
-
-const TestBox = styled.div`
-    height: 100px;
-    width: 100px;
-    background-color: blue;
-`
-
-const LineContainer = styled.div`
-    width: 80%;
-    svg {
-        path {
-            width: 100%;
-        }
-    }
 `
 
 export default SkillLine
