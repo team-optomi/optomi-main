958b7931e5077c22c6f02a49bb75cfda330d1570
Jason <jason@theremotecreative.com>
2020-08-21 15:28:45 -0500

    testing gsap v3

--- src/components/home-sections/skill-focused-line.js
+++ src/components/home-sections/skill-focused-line.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import styled from 'styled-components'
-import { TimelineLite } from 'gsap';
+import { TweenLite, TimelineLite } from 'gsap';
 
 class SkillLine extends Component {
 
