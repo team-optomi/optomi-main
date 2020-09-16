d48669cc4741c83f429e63dc734d2e510c205259
Jason <jason@theremotecreative.com>
2020-09-15 19:11:19 -0500

    new services page

--- src/pages/service-offerings.js
+++ src/pages/service-offerings.js
@@ -111,7 +111,7 @@ const ServicesPage = () => {
                     data-sal-delay="600"
                     data-sal-easing="ease"
                     >
-                        <HeroCopy 
+                        <SecondCopy 
                             dangerouslySetInnerHTML={{ __html: post.node.acf.second_paragraph }}
                         />
                     </div>
@@ -154,37 +154,34 @@ const ServicesPage = () => {
 
 const HeroBanner = styled.div`
     position: relative; 
+    min-height: 100vh;
+    min-width: 100%;
+    height: 100%;
     width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: flex-end;
+    z-index: 2;
+    @media (max-width:600px) {
+        display: block;
+    }
 ` 
 
-const HeroImg = styled(Img)`
-    height: 100%;
-`
-
-const BackgroundImg = styled(Img)`
-    @media(max-width:1000px) {
-        height: 100%;
-    }
-`
-
 const HeroBackground = styled.div`
+    background-color: #000;
     position: absolute;
+    height: 100vh;
     width: 100%;
-    height: 100%;
     top: 0;
     left: 0;
-    z-index: 1;
 `
 
-const ImageBackground = styled.div`
-    position: relative;
-    z-index: 1;
-    @media(max-width:1000px) {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        left: 0;
+const HeroImg = styled(Img)`
+    height: 100vh;
+    width: 100%;
+    img {
+        margin-bottom: 0;
     }
 `
 
@@ -193,23 +190,27 @@ const HeroContent = styled.div`
     width: 100%;
     display: flex;
     flex-wrap: wrap;
-    justify-content: center;
-    align-items: flex-end;
-    padding-top: 300px;
+    justify-content: flex-end;
+    align-items: center;
+    padding-top: 0px;
     padding-bottom: 50px;
     z-index: 2;
     > div {
-        max-width: 1340px;
+        max-width: 1540px;
         width: 100%;
-        padding-left: 20px;
-        padding-right: 20px;
+        padding-left: 120px;
+        padding-right: 120px;
         margin: 0 auto;
         transition-duration: .3s;
         @media(max-width:1500px) {
-            max-width: 1140px;
+            max-width: 1340px;
         }
         @media(max-width:1200px) {
-            max-width: 940px;
+            max-width: 1140px;
+        }
+        @media(max-width:700px) {
+            padding-left: 20px;
+            padding-right: 20px;
         }
     }
     @media(max-width:1200px) {
@@ -220,6 +221,42 @@ const HeroContent = styled.div`
     }
     @media(max-width:500px) {
         padding-top: 30px;
+    }
+`
+
+const HeroCopy = styled.div`
+    max-width: 900px;
+    width: 100%;
+    margin-left: auto;
+    text-align: left;
+    p {
+        font-family: "Helvetica Thin";
+        width: 100%;
+        z-index: 1;
+        font-size: 24px;
+        color: #fff;
+        line-height: 1.2;
+        transition-duration: .3s;
+        span.bonvivant {
+            font-family: "BonVivant";
+            color: #fff;
+            font-size: 39px;
+            font-weight: 100;
+            line-height: 1.7;
+        }
+    }
+    @media(max-width:1160px) {
+        text-align: center;
+        max-width: 700px;
+        margin-right: auto;
+    }
+    @media(max-width:800px) {
+        p {
+            font-size: 20px;
+            span.bonvivant {
+                font-size: 32px;
+            }
+        }
     }
 `
 
@@ -312,52 +349,8 @@ const SecondParagraph = styled.div`
     }
 `
 
-const HeroTitle = styled.div`
-    position: absolute;
-    top: 100px;
-    left: 150px;
-    z-index: 2;
-    h1 {
-        font-family: "Helvetica Thin";
-        color: #5ab3e8;
-        margin-bottom: 0;
-        text-transform: lowercase;
-        font-size: 72px;
-        font-weight: 100;
-        line-height: 1;
-        @media(max-width:1200px) {
-            font-size: 62px;
-        }
-        @media(max-width:850px) {
-            font-size: 52px;
-        }
-    }
-    p {
-        font-family: "Helvetica Thin";
-        letter-spacing: 2px;
-        width: 100%;
-        text-align: center;
-        z-index: 1;
-        font-size: 20px;
-        color: rgb(140,145,146);
-        span {
-            color: #5ab3e8;
-        }
-        @media(max-width:850px) {
-            font-size: 18px;
-        }
-    }
-    @media(max-width: 850px) {
-        position: relative;
-        top: auto;
-        left: auto;
-        padding: 20px;
-        padding-top: 100px;
-        text-align: center;
-    }
-`
 
-const HeroCopy = styled.div`
+const SecondCopy = styled.div`
     max-width: 780px;
     width: 100%;
     margin: 0 auto;
@@ -404,6 +397,70 @@ const HeroCopy = styled.div`
         }
     }
 `
+
+const HeroTitle = styled.div`
+    position: absolute;
+    top: 100px;
+    left: 150px;
+    z-index: 2;
+    h1 {
+        font-family: "Helvetica Thin";
+        color: #5ab3e8;
+        margin-bottom: 0;
+        text-transform: lowercase;
+        font-size: 72px;
+        font-weight: 100;
+        line-height: 1;
+        @media(max-width:1200px) {
+            font-size: 62px;
+        }
+        @media(max-width:850px) {
+            font-size: 52px;
+        }
+    }
+    p {
+        font-family: "Helvetica Thin";
+        letter-spacing: 2px;
+        width: 100%;
+        text-align: center;
+        z-index: 1;
+        font-size: 20px;
+        color: rgb(140,145,146);
+        span {
+            color: #5ab3e8;
+        }
+        @media(max-width:850px) {
+            font-size: 18px;
+        }
+    }
+    @media(max-width: 850px) {
+        position: relative;
+        top: auto;
+        left: auto;
+        padding: 20px;
+        padding-top: 100px;
+        text-align: center;
+    }
+`
+
+const ImageBackground = styled.div`
+    position: relative;
+    z-index: 1;
+    @media(max-width:1000px) {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+    }
+`
+
+const BackgroundImg = styled(Img)`
+    @media(max-width:1000px) {
+        height: 100%;
+    }
+`
+
 
 const ServiceBanner = styled.div`
     position: relative; 
