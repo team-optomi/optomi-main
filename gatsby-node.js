9964c13ed18eba12e19ca2f1c4fa188000ffb7ae
team-optomi <j.vanderheyden86@gmail.com>
2020-10-12 08:17:58 -0500

    redirect testv8

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,8 +10,13 @@ const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createRedirect } = actions
-  createRedirect({ fromPath: '/press', toPath: 'https://press.optomi.com', isPermanent: true })
+  createRedirect({ fromPath: '/press/*', toPath: 'https://press.optomi.com', isPermanent: true })
+  createRedirect({ fromPath: '/opportunities/*', toPath: 'https://opportunities.optomi.com', isPermanent: true })
+  createRedirect({ fromPath: '/giving-back/*', toPath: 'https://giving-back.optomi.com', isPermanent: true })
+  createRedirect({ fromPath: '/consultant-community-blog/*', toPath: 'https://consultant-community-blog.optomi.com', isPermanent: true })
+  createRedirect({ fromPath: '/optomi-opportunities/*', toPath: 'http://optomi-opportunities.optomi.com', isPermanent: true })
   createRedirect({ fromPath: '/services', toPath: '/service-offerings', isPermanent: true })
+  createRedirect({ fromPath: '/jobs/#/jobs', toPath: 'https://jobs.optomi.com/jobs/#/jobs', isPermanent: true })
   const { createPage } = actions
   const BlogPostTemplate = path.resolve("./src/templates/post.js")
   const CaseStudyTemplate = path.resolve("./src/templates/case-study.js")
